6768357babf6b67c4ca37a92ebb0b3efe9375597
Jakub Kozłowski <69166301+jakubkozlowski1104@users.noreply.github.com>
2024-10-12 18:50:32 +0200

    Project created

---
+++ backend/travelBuddy/src/main/java/travelBuddy/TravelBuddyApplication.java
@@ -0,0 +1,13 @@
+package travelBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBuddyApplication.class, args);
+	}
+
+}
