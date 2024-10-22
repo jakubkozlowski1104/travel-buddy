b3bae2670102f4ba93ed70f3cb43e28fc6c8d10b
Jakub Kozłowski <69166301+jakubkozlowski1104@users.noreply.github.com>
2024-10-22 08:55:33 +0200

    Create model User, update Java version

---
+++ backend/travelBuddy/src/main/java/com/travelBuddy/model/User.java
@@ -0,0 +1,96 @@
+package com.travelBuddy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String username;
+    private String email;
+    private String bio;
+    private String photoUrl;
+    private String interests;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getInterests() {
+        return interests;
+    }
+
+    public void setInterests(String interests) {
+        this.interests = interests;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
