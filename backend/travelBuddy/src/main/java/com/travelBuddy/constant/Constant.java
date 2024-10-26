e3a9cf0d6bff709b097befd34a3e1c11ff712bc5
Jakub Kozłowski <69166301+jakubkozlowski1104@users.noreply.github.com>
2024-10-26 19:04:16 +0200

    handle photo in UserService

---
+++ backend/travelBuddy/src/main/java/com/travelBuddy/constant/Constant.java
@@ -0,0 +1,5 @@
+package com.travelBuddy.constant;
+
+public class Constant {
+    public static final String PHOTO_DIRECTORY = System.getenv("user.home") + "/Downloads/uploads";
+}
--- backend/travelBuddy/src/main/java/com/travelBuddy/service/UserService.java
+++ backend/travelBuddy/src/main/java/com/travelBuddy/service/UserService.java
@@ -7,10 +7,20 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static com.travelBuddy.constant.Constant.PHOTO_DIRECTORY;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 @Service
 @Slf4j
@@ -36,4 +46,33 @@ public class UserService {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
+
+    public String uploadPhoto(String id, MultipartFile file) throws Exception {
+        log.info("Saving picture for user ID: ", id);
+        UUID userId = UUID.fromString(id);
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        String photoUrl = photoFunction.apply(id, file);
+        user.setPhotoUrl(photoUrl);
+        userRepository.save(user);
+        return photoUrl;
+    }
+
+    private final Function<String, String> fileExtension = filename -> Optional.of(filename).filter(name -> name.contains("."))
+            .map(name -> "." + name.substring(filename.lastIndexOf(".") + 1)).orElse(".png");
+
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        String filename = id + fileExtension.apply(image.getOriginalFilename());
+        try {
+            Path fileStorageLocation = Paths.get(PHOTO_DIRECTORY).toAbsolutePath().normalize();
+            if (!Files.exists(fileStorageLocation)) {
+                Files.createDirectories(fileStorageLocation);
+            }
+            Files.copy(image.getInputStream(), fileStorageLocation.resolve(filename), REPLACE_EXISTING);
+            return ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/contacts/image/" + filename).toString();
+        } catch (Exception exception) {
+            throw new RuntimeException("Unable to save image");
+        }
+    };
 }
