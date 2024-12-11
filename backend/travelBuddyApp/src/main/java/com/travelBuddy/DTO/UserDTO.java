6af2cfcb2878fa95e25623eceb6e73c40ecc3c5c
Jakub Kozłowski <69166301+jakubkozlowski1104@users.noreply.github.com>
2024-12-11 10:08:49 +0100

    cleaning and push

--- backend/travelBuddyApp/src/main/java/com/travelBuddy/DTO/UserDTO.java
+++ backend/travelBuddyApp/src/main/java/com/travelBuddy/DTO/UserDTO.java
@@ -12,13 +12,13 @@ public class UserDTO {
     private String id;
     private String username;
     private String email;
-    private String password; // Może być ukrywane w odpowiedziach, zależnie od wymagań
+    private String password;
     private String bio;
     private String photoUrl;
     private String interests;
     private int age;
     private String country;
     private LocalDateTime createdAt;
-    private List<String> countriesVisited; //names
+    private List<String> countriesVisited;
 }
 
