016943f2cbad40cf324c4f7b03136e25286993de
Jakub Kozłowski <69166301+jakubkozlowski1104@users.noreply.github.com>
2024-12-11 16:02:14 +0100

    before changes

--- frontend/travelBuddyFrontend/src/components/RecentTrips/RecentTrips.jsx
+++ frontend/travelBuddyFrontend/src/components/RecentTrips/RecentTrips.jsx
@@ -25,49 +25,21 @@ const RecentTrips = () => {
           </h3>
         </div>
         <div className="trips-container">
-          <div className="item">
-            <div className="img">
-              <img src="/travelTypes/nomad_style.jpg" alt="" />
-            </div>
-            <div className="details">
-              <div className="top-info">
-                <h1>trip name</h1>
-              </div>
-              <div className="bottom-info">
-                <div className="left-info">
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>Kategoria</p>
-                  </div>
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>12 days</p>
-                  </div>
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>Thailand</p>
-                  </div>
-                </div>
-                <div className="right-info">
-                  <p className="price-name">PRICE</p>
-                  <p className="price">
-                    1500 <i>E</i>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+
+
 
           <div className="item">
             <div className="time-left">12 DAYS LEFT</div>
-
             <div className="img">
               <img src="/travelTypes/test.jpg" alt="" />
             </div>
             <div className="details">
               <div className="user">
                 <div className="icon">
-                  <img src="/users/userimg.jpg" alt="" />
+                  <img
+                    src="/usersPhotos/c69d9ad8-ed33-4437-b230-2c6bd587589f.jpg"
+                    alt=""
+                  />
                 </div>
                 <div className="name">
                   Kuba_koz | <span className="smaller">Poland</span> |{' '}
@@ -105,133 +77,6 @@ const RecentTrips = () => {
                   </p>
                   <p className="price">
                     2100<i>€</i>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="item">
-            <div className="img">
-              <img src="/travelTypes/test.jpg" alt="" />
-            </div>
-            <div className="details">
-              <div className="top-info">
-                <h1>trip name</h1>
-              </div>
-              <div className="bottom-info">
-                <div className="left-info">
-                  <div className="more-info">
-                    <FontAwesomeIcon icon={faLocationDot} />
-                    <i></i>
-                    <p>Kategoria</p>
-                  </div>
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>12 days</p>
-                  </div>
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>Thailand</p>
-                  </div>
-                </div>
-                <div className="right-info">
-                  <p className="price-name"> BUDGET ESTIMATED</p>
-                  <p className="price">
-                    2100<i>€</i>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="item">
-            <div className="img">
-              <img src="/travelTypes/nomad_style.jpg" alt="" />
-            </div>
-            <div className="details">
-              <div className="top-info">
-                <h1>trip name</h1>
-              </div>
-              <div className="bottom-info">
-                <div className="left-info">
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>Kategoria</p>
-                  </div>
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>12 days</p>
-                  </div>
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>Thailand</p>
-                  </div>
-                </div>
-                <div className="right-info">
-                  <p className="price-name">ESTIMATED PRICE</p>
-                  <p className="price">
-                    1500 <i>E</i>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="item">
-            <div className="img">
-              <img src="/travelTypes/nomad_style.jpg" alt="" />
-            </div>
-            <div className="details">
-              <div className="top-info">
-                <h1>trip name</h1>
-              </div>
-              <div className="bottom-info">
-                <div className="left-info">
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>Kategoria</p>
-                  </div>
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>12 days</p>
-                  </div>
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>Thailand</p>
-                  </div>
-                </div>
-                <div className="right-info">
-                  <p className="price-name">ESTIMATED PRICE</p>
-                  <p className="price">
-                    1500 <i>E</i>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="item">
-            <div className="img">
-              <img src="/travelTypes/nomad_style.jpg" alt="" />
-            </div>
-            <div className="details">
-              <h1>trip name</h1>
-              <div className="bottom-info">
-                <div className="left-info">
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>Kategoria</p>
-                  </div>
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>12 days</p>
-                  </div>
-                  <div className="more-info">
-                    <i>X</i>
-                    <p>Thailand</p>
-                  </div>
-                </div>
-                <div className="right-info">
-                  <p className="price-name"> PRICE</p>
-                  <p className="price">
-                    1500 <i>E</i>
                   </p>
                 </div>
               </div>
