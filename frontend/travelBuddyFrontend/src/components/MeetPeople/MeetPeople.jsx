05f65a6e21eb54e9ab12ef59d50f28ffc354b207
Jakub Kozłowski <69166301+jakubkozlowski1104@users.noreply.github.com>
2024-12-22 18:42:29 +0100

    basic styles

--- frontend/travelBuddyFrontend/src/components/MeetPeople/MeetPeople.jsx
+++ frontend/travelBuddyFrontend/src/components/MeetPeople/MeetPeople.jsx
@@ -4,7 +4,69 @@ import { StyledMeetContent } from './MeetPeople.styles';
 const MeetPeople = () => {
   return (
     <StyledMeetContent id="meet">
-      <div>siema</div>
+      <div className="center">
+        <div className="people">
+          <div className="person">
+            <div className="top">
+              <img src="" alt="" />
+              <div className="user-info">User, 23</div>
+            </div>
+            <div className="bottom">
+              <div className="chat">
+                <i>X</i>chat
+              </div>
+              <div className="wave">
+                <i>X</i>wave
+              </div>
+            </div>
+          </div>
+
+          <div className="person">
+            <div className="top">
+              <img src="" alt="" />
+              <div className="user-info">userek, 23</div>
+            </div>
+            <div className="bottom">
+              <div className="chat">
+                <i>X</i>chat
+              </div>
+              <div className="wave">
+                <i>X</i>wave
+              </div>
+            </div>
+          </div>
+
+          <div className="person">
+            <div className="top">
+              <img src="" alt="" />
+              <div className="user-info">userek, 23</div>
+            </div>
+            <div className="bottom">
+              <div className="chat">
+                <i>X</i>chat
+              </div>
+              <div className="wave">
+                <i>X</i>wave
+              </div>
+            </div>
+          </div>
+
+          <div className="person">
+            <div className="top">
+              <img src="" alt="" />
+              <div className="user-info">userek, 23</div>
+            </div>
+            <div className="bottom">
+              <div className="chat">
+                <i>X</i>chat
+              </div>
+              <div className="wave">
+                <i>X</i>wave
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </StyledMeetContent>
   );
 };
--- frontend/travelBuddyFrontend/src/components/MeetPeople/MeetPeople.styles.jsx
+++ frontend/travelBuddyFrontend/src/components/MeetPeople/MeetPeople.styles.jsx
@@ -6,4 +6,54 @@ export const StyledMeetContent = styled.div`
   align-items: center;
   flex: 1;
   font-family: 'Lato', sans-serif;
+  background-color: red;
+  width: 100%;
+  margin: 40px;
+
+  .center {
+    position: relative;
+    z-index: 3;
+    width: 300px;
+    height: 280px;
+    width: 66%;
+    justify-content: center;
+    margin: auto;
+
+    .people {
+      display: flex;
+      gap: 35px;
+      height: 100%;
+
+      .person {
+        display: flex;
+        flex-direction: column;
+        background-color: blue;
+        width: 400px;
+        height: 100%;
+
+        .top {
+          position: relative;
+          flex: 1;
+          background-color: yellow;
+
+          .user-info {
+            position: absolute;
+            bottom: 3px;
+            left: 5px;
+          }
+        }
+
+        .bottom {
+          display: flex;
+          .chat,
+          .wave {
+            padding: 5px;
+            border: 1px solid red;
+            flex: 1;
+            text-align: center;
+          }
+        }
+      }
+    }
+  }
 `;
