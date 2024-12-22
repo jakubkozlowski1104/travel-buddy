0fb475e09f32ee712c3247b25ea0c90d4a372583
Jakub Kozłowski <69166301+jakubkozlowski1104@users.noreply.github.com>
2024-12-22 18:10:22 +0100

    final

--- frontend/travelBuddyFrontend/src/components/StartTripBanner/StartTripBanner.jsx
+++ frontend/travelBuddyFrontend/src/components/StartTripBanner/StartTripBanner.jsx
@@ -5,13 +5,12 @@ import logo from '../../assets/logo.png';
 import { useTranslation } from 'react-i18next';
 
 const StartTripBanner = () => {
-  const { t } = useTranslation(); // Inicjalizacja tłumaczeń
+  const { t } = useTranslation();
 
   return (
     <StyledStartContainer>
       <div className="img">
         <img src="/bannerJoinTrip.jpg" alt={t('startTrip.days')} />{' '}
-        {/* Dodane tłumaczenie */}
       </div>
       <div className="center">
         <div className="img-logo">
@@ -19,16 +18,12 @@ const StartTripBanner = () => {
         </div>
         <div className="info">
           <div className="slogan">{t('startTrip.header')}</div>{' '}
-          {/* Tłumaczenie nagłówka */}
-          <div className="description">
-            {t('startTrip.description')} {/* Tłumaczenie sloganu */}
-          </div>
+          <div className="description">{t('startTrip.description')}</div>
           <div className="bottom">
             <Button color="secondary" variant="contained" className="btn">
-              {t('startTrip.button')} {/* Tłumaczenie przycisku */}
+              {t('startTrip.button')}
             </Button>
             <div className="text">{t('startTrip.note')}</div>{' '}
-            {/* Tłumaczenie tekstu */}
           </div>
         </div>
       </div>
