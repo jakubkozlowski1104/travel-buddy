d36cbdb839a30ec11caa4375cec3f7149fd89724
Jakub Kozłowski <69166301+jakubkozlowski1104@users.noreply.github.com>
2024-12-22 18:19:08 +0100

    ceate component, add to mainpage

---
+++ frontend/travelBuddyFrontend/src/components/MeetPeople/MeetPeople.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledMeetContent } from './MeetPeople.styles';
+
+const MeetPeople = () => {
+  return (
+    <StyledMeetContent id="meet">
+      <div>siema</div>
+    </StyledMeetContent>
+  );
+};
+
+export default MeetPeople;
---
+++ frontend/travelBuddyFrontend/src/components/MeetPeople/MeetPeople.styles.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const StyledMeetContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  font-family: 'Lato', sans-serif;
+`;
