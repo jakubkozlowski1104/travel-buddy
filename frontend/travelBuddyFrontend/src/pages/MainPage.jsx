bf0fdedaddd10e120fb791e8ffe1578e04c60813
Jakub Kozłowski <69166301+jakubkozlowski1104@users.noreply.github.com>
2024-12-23 21:42:15 +0100

    Add search bar

--- frontend/travelBuddyFrontend/src/pages/MainPage.jsx
+++ frontend/travelBuddyFrontend/src/pages/MainPage.jsx
@@ -6,13 +6,35 @@ import StartTripBanner from '../components/StartTripBanner/StartTripBanner';
 import MeetPeople from '../components/MeetPeople/MeetPeople';
 import HowItWorks from '../components/HowItWorks/HowItWorks';
 import Footer from '../components/Footer/Footer';
-
+import { Button } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const MainPage = () => {
   return (
     <StyledContainer>
       <section className="start" id="start">
         <img src={backgroundImage} alt="foto" className="background-image" />
         <div className="overlay"></div>
+        <div className="center">
+          <div className="info">
+            <div className="slogan">FIND A TRAVEL BUDDY</div>
+            <div className="description">
+              Going on a trip? Meet people who love to travel. <br /> Share your
+              adventures with a travel companion.
+            </div>
+            <form action="">
+              <div className="search-bar">
+                <i className="search-icon">
+                  <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </i>
+                <input type="text" placeholder="Search trip destination" />
+                <Button color="own" variant="contained" className="btn">
+                  SEARCH
+                </Button>
+              </div>
+            </form>
+          </div>
+        </div>
       </section>
 
       <TravelStyle />
--- frontend/travelBuddyFrontend/src/pages/MainPageStyles.jsx
+++ frontend/travelBuddyFrontend/src/pages/MainPageStyles.jsx
@@ -36,15 +36,86 @@ export const StyledContainer = styled.div`
       left: 0;
       width: 100%;
       height: calc(100vh + 20vh); /* Dopasowanie do obrazu */
-      background-color: rgba(0, 0, 0, 0.3);
+      background-color: rgba(0, 0, 0, 0.4);
       z-index: 10;
     }
-  }
 
-  .footer {
-    background-color: blue;
-    width: 100%;
-    height: 15vh;
-    margin-top: auto; /* Stopka zawsze na dole */
+    .center {
+      width: 66%;
+      height: 85vh;
+      z-index: 10;
+      top: 100px;
+      width: 65.5%;
+      margin: auto;
+
+      .info {
+        color: white;
+        display: flex;
+        height: 100%;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-left: 10px;
+        gap: 0px;
+
+        .slogan {
+          font-size: 4rem;
+          font-weight: bold;
+        }
+
+        .description {
+          font-size: 1.7rem;
+          line-height: 1.2;
+        }
+
+        form {
+          position: absolute;
+          bottom: 7vh;
+          display: flex;
+          justify-content: center;
+          align-items: baseline;
+
+          .search-bar {
+            display: flex;
+            align-items: center;
+            background: linear-gradient(90deg, #004475, #4db8d3);
+            border-radius: 20px;
+            padding: 10px 30px;
+            width: 800px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            font-size: 1.5rem;
+
+            .search-icon {
+              font-size: 30px;
+              color: white;
+              margin-right: 20px;
+            }
+
+            input {
+              border: none;
+              outline: none;
+              background: transparent;
+              color: white;
+              font-size: 1.6rem;
+              width: 100%;
+              font-family: 'Lato', sans-serif;
+
+              &::placeholder {
+                color: rgba(255, 255, 255, 0.8);
+              }
+            }
+          }
+
+          .btn {
+            background-color: #43d0d7;
+            margin-right: -20px;
+            border-radius: 15px;
+            padding: 12px 25px;
+            font-weight: bold;
+            font-size: 1.1rem;
+          }
+        }
+      }
+    }
   }
 `;
