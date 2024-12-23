eae928e3fc371221467d78915c858367d5aed122
Jakub Kozłowski <69166301+jakubkozlowski1104@users.noreply.github.com>
2024-12-23 09:54:14 +0100

    add icons

--- frontend/travelBuddyFrontend/src/components/MeetPeople/MeetPeople.jsx
+++ frontend/travelBuddyFrontend/src/components/MeetPeople/MeetPeople.jsx
@@ -1,70 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyledMeetContent } from './MeetPeople.styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment } from '@fortawesome/free-regular-svg-icons'; // Zmieniono na 'free-regular-svg-icons'
+import { faHand } from '@fortawesome/free-regular-svg-icons'; // Zmieniono na 'free-regular-svg-icons'
 
 const MeetPeople = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // Fetch users from backend
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:8080/users?page=0&size=6'
+        ); // Backend endpoint
+        const data = await response.json();
+        setUsers(data.content || []); // Assuming response has a "content" key for paginated results
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <StyledMeetContent id="meet">
       <div className="center">
         <div className="people">
-          <div className="person">
-            <div className="top">
-              <img src="" alt="" />
-              <div className="user-info">User, 23</div>
-            </div>
-            <div className="bottom">
-              <div className="chat">
-                <i>X</i>chat
+          {users.map((user) => (
+            <div className="person" key={user.id}>
+              <div className="top">
+                <img
+                  src={user.photoUrl || '/default-avatar.png'}
+                  alt={`${user.username}`}
+                />
+                <div className="user-info">
+                  {user.username}, {user.age}
+                </div>
               </div>
-              <div className="wave">
-                <i>X</i>wave
-              </div>
-            </div>
-          </div>
-
-          <div className="person">
-            <div className="top">
-              <img src="" alt="" />
-              <div className="user-info">userek, 23</div>
-            </div>
-            <div className="bottom">
-              <div className="chat">
-                <i>X</i>chat
-              </div>
-              <div className="wave">
-                <i>X</i>wave
+              <div className="bottom">
+                <div className="chat">
+                  <i>
+                    <FontAwesomeIcon icon={faComment} />
+                  </i>{' '}
+                  chat
+                </div>
+                <div className="wave">
+                  <FontAwesomeIcon icon={faHand} /> wave
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="person">
-            <div className="top">
-              <img src="" alt="" />
-              <div className="user-info">userek, 23</div>
-            </div>
-            <div className="bottom">
-              <div className="chat">
-                <i>X</i>chat
-              </div>
-              <div className="wave">
-                <i>X</i>wave
-              </div>
-            </div>
-          </div>
-
-          <div className="person">
-            <div className="top">
-              <img src="" alt="" />
-              <div className="user-info">userek, 23</div>
-            </div>
-            <div className="bottom">
-              <div className="chat">
-                <i>X</i>chat
-              </div>
-              <div className="wave">
-                <i>X</i>wave
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </StyledMeetContent>
--- frontend/travelBuddyFrontend/src/components/MeetPeople/MeetPeople.styles.jsx
+++ frontend/travelBuddyFrontend/src/components/MeetPeople/MeetPeople.styles.jsx
@@ -6,7 +6,6 @@ export const StyledMeetContent = styled.div`
   align-items: center;
   flex: 1;
   font-family: 'Lato', sans-serif;
-  background-color: red;
   width: 100%;
   margin: 40px;
 
@@ -14,32 +13,39 @@ export const StyledMeetContent = styled.div`
     position: relative;
     z-index: 3;
     width: 300px;
-    height: 280px;
+    height: 200px;
     width: 66%;
     justify-content: center;
     margin: auto;
 
     .people {
       display: flex;
-      gap: 35px;
+      gap: 0px;
       height: 100%;
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
 
       .person {
         display: flex;
         flex-direction: column;
-        background-color: blue;
         width: 400px;
         height: 100%;
-
         .top {
           position: relative;
+          display: flex;
+          justify-content: center;
           flex: 1;
-          background-color: yellow;
+          overflow: hidden;
+
+          img {
+            width: 93%;
+          }
 
           .user-info {
             position: absolute;
             bottom: 3px;
-            left: 5px;
+            left: 15px;
+            color: white;
+            font-size: 1.3rem;
           }
         }
 
@@ -48,9 +54,23 @@ export const StyledMeetContent = styled.div`
           .chat,
           .wave {
             padding: 5px;
-            border: 1px solid red;
             flex: 1;
             text-align: center;
+            font-size: 1.1rem;
+          }
+
+          .wave {
+            position: relative;
+          }
+
+          .wave:after {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 90%;
+            width: 3px;
+            background-color: #6fceee;
           }
         }
       }
